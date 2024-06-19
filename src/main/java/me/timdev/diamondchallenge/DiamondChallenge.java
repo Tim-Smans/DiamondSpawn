44389db1737e7bc9b6821c21d07c2edc95cc307d
TMTimSmans <r0994452@student.thomasmore.be>
2024-06-20 01:20:47 +0200

    Added ChunkEvents, ability to check if chunk is newly generated.

--- src/main/java/me/timdev/diamondchallenge/DiamondChallenge.java
+++ src/main/java/me/timdev/diamondchallenge/DiamondChallenge.java
@@ -1,17 +1,25 @@
 package me.timdev.diamondchallenge;
 
+import me.timdev.diamondchallenge.events.ChunkEvents;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DiamondChallenge extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        Bukkit.getLogger().info("Plugin successfully started! Stinky.");
+
+        registerEvents();
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public void registerEvents(){
+        Bukkit.getServer().getPluginManager().registerEvents(new ChunkEvents(), this);
     }
 }
--- src/main/java/me/timdev/diamondchallenge/events/ChunkEvents.java
+++ src/main/java/me/timdev/diamondchallenge/events/ChunkEvents.java
@@ -1,4 +1,23 @@
 package me.timdev.diamondchallenge.events;
 
-public class ChunkEvents {
+import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.world.ChunkLoadEvent;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ChunkEvents implements Listener {
+
+
+    @EventHandler
+    public void onChunkLoad(ChunkLoadEvent event) {
+        if (!event.isNewChunk()) {
+            return;
+        }
+        Bukkit.getLogger().info("Nieuwe chunk gevonden.");
+    }
 }
